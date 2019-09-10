f9bd90b8ead043bf15413bf5bc6aa9f33fb479a6
skeller88 <skeller88@gmail.com>
2019-09-09 23:34:56 -0700

    t

--- data_engineering/gcs_downloader.py
+++ data_engineering/gcs_downloader.py
@@ -11,12 +11,12 @@ def main(bucket_name, blob_name):
     _bucket = gcs_client.bucket(bucket_name)
     _blob = _bucket.blob(blob_name)
 
-    with open(Path.home() / 'Documents/big_earth_springboard_project/test_archive_downloaded.tar.gz', 'wb') as fileobj:
+    with open(Path.home() / 'Documents/big_earth_springboard_project/test_downloaded.tif', 'wb') as fileobj:
         resp = _blob.download_as_string()
         fileobj.write(resp)
 
 
 bucket_name: str = "big_earth"
-blob_name: str = "test-blob"
+blob_name: str = "raw/S2A_MSIL2A_20170717T113321_10_80/S2A_MSIL2A_20170717T113321_10_80_B01.tif"
 
 main(bucket_name, blob_name)
--- data_engineering/metadata_reader/main.py
+++ data_engineering/metadata_reader/main.py
@@ -1,5 +1,8 @@
 import csv
+import json
 import os
+import time
+
 import tarfile
 
 from google.cloud import storage
@@ -12,6 +15,7 @@ def main():
     bucket_name: str = os.environ.get("GCS_BUCKET_NAME")
     metadata_blob_name: str = os.environ.get("GCS_METADATA_BLOB_NAME")
     tarfile_blob_name: str = os.environ.get("GCS_TARFILE_BLOB_NAME")
+    uncompressed_blob_prefix: str = os.environ.get("UNCOMPRESSED_BLOB_PREFIX")
     disk_path: str = os.environ.get("DISK_PATH")
 
     tarfile_disk_path: str = disk_path + "/" + tarfile_blob_name
@@ -21,7 +25,8 @@ def main():
               f"{tarfile_disk_path}")
 
         print("disk_path contents before download", os.listdir(disk_path))
-        with GCSObjectStreamDownloader(client=gcs_client, bucket_name=bucket_name, blob_name=tarfile_blob_name) as gcs_downloader:
+        with GCSObjectStreamDownloader(client=gcs_client, bucket_name=bucket_name,
+                                       blob_name=tarfile_blob_name) as gcs_downloader:
             print("tarfile_disk_path", tarfile_disk_path)
             with open(tarfile_disk_path, 'wb') as fileobj:
                 chunk = gcs_downloader.read()
@@ -31,16 +36,57 @@ def main():
 
         print("disk_path contents after download", os.listdir(disk_path))
 
-    with tarfile.open(tarfile_disk_path, 'r') as fileobj:
-        fileobj.extractall(path=disk_path)
+    extraction_path = tarfile_disk_path.replace(".gz", "").replace(".tar", "")
 
-    print(f"Extracted tar")
+    if os.environ.get("SHOULD_EXTRACT_TARFILE") == "True":
+        with tarfile.open(tarfile_disk_path, 'r') as fileobj:
+            fileobj.extractall(path=disk_path)
+        print(f"tar extracted from {tarfile_disk_path} to {extraction_path}")
 
+    metadata_file = disk_path + "/" + "image_metadata.csv"
     bucket = gcs_client.bucket(bucket_name)
+
+    num_files_uploaded = 0
+    num_folders_uploaded = 0
+    checkpoint = 1000
+    start = time.time()
+    with open(metadata_file, "w") as metadata_fileobj:
+        writer = csv.DictWriter(metadata_fileobj,
+                                fieldnames=["image_prefix", "labels", "coordinates", "projection", "tile_source",
+                                            "acquisition_date"])
+
+        # Don't use walk because filenames will have thousands of files. Iterate one by one instead
+        for subdir_name in os.listdir(extraction_path):
+            subdir_path = extraction_path + "/" + subdir_name
+            if os.path.isdir(subdir_path):
+                for filename in os.listdir(subdir_path):
+                    if not filename.startswith("._"):
+                        file_suffix = filename.rsplit(".")[-1]
+
+                        content_type = "image/tiff" if file_suffix == ".tif" else "application/json"
+                        blob_name: str = os.path.join(uncompressed_blob_prefix, subdir_name, filename)
+                        print("blob_name", blob_name)
+                        blob = bucket.blob(blob_name)
+
+                        filepath = subdir_path + "/" + filename
+                        blob.upload_from_filename(filepath, content_type=content_type)
+                        num_files_uploaded += 1
+                        if filename.endswith('.json'):
+                            with open(filepath) as fileobj:
+                                json_obj = json.loads(fileobj.read())
+                                json_obj["image_prefix"] = subdir_name
+                                writer.writerow(json_obj)
+                num_folders_uploaded += 1
+
+            if num_files_uploaded > checkpoint:
+                elapsed = (time.time() - start) / 60
+                print(f"Uploaded {num_files_uploaded} files and {num_folders_uploaded} folders in {elapsed} minutes, {num_files_uploaded / elapsed} files per minute.")
+                checkpoint += 1000
+
     blob = bucket.blob(metadata_blob_name)
-    blob.upload_from_filename(disk_path + "/temp_names.csv")
+    blob.upload_from_filename(metadata_file)
+    print(f"Uploaded metadata to {metadata_blob_name}")
+
 
 if __name__ == "__main__":
     main()
-
-
